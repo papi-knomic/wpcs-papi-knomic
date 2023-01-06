c3dfe8ae522e7f9e71a3871e07cefcb9fd025f47
Samson Moses <knomic10@gmail.com>
2023-01-06 17:45:05 +0100

    Resolved issues with code in Enqueue.php based on bot suggestion

--- includes/Base/Enqueue.php
+++ includes/Base/Enqueue.php
@@ -9,17 +9,16 @@ class Enqueue extends BaseController {
 	}
 
 	public function printScripts() : void {
-
 		if ( ! is_admin() ){
 			//register
-			wp_register_script('slideshow', $this->plugin_url . 'assets/js/slideshow.js');
+			wp_register_script('slideshow', $this->plugin_url . 'assets/js/slideshow.js',  [], '1.0', false);
 
 			// enqueue frontend scripts
 			wp_enqueue_script('slideshow');
 		}
 
 		//register general scripts
-		wp_register_script('jquery', 'https://code.jquery.com/jquery-3.6.0.js');
+		wp_register_script('jquery', 'https://code.jquery.com/jquery-3.6.0.js', [], '1.0', false);
 
 		wp_enqueue_script('jquery');
 		wp_enqueue_script('jquery-ui-datepicker' );
