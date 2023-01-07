e73665c76045810a2a6e71e71bd910c4616fa54d
Samson Moses <knomic10@gmail.com>
2023-01-07 19:43:36 +0100

    Escaped strings in admin-page

--- views/admin-page.php
+++ views/admin-page.php
@@ -7,7 +7,7 @@
             <label for="upload-slideshow-images"><?php esc_attr_e('Add Slideshow Images', 'slideshow'); ?></label>
         </th>
         <td>
-            <button type="button" class="button button-secondary" id="upload-slideshow-images" data-multiple="true" data-button-text="<?php esc_attr_e('Add to Gallery', 'slideshow'); ?>" data-title="<?php _e('Add Images to Gallery', 'meta-slider-and-carousel-with-lightbox'); ?>"><i class="dashicons dashicons-format-gallery"></i> <?php _e('Gallery Images', 'meta-slider-and-carousel-with-lightbox'); ?></button>
+            <button type="button" class="button button-secondary" id="upload-slideshow-images" data-multiple="true" data-button-text="<?php esc_attr_e('Add to Gallery', 'slideshow'); ?>" data-title="<?php esc_attr_e('Add Images to Gallery', 'slideshow'); ?>"><i class="dashicons dashicons-format-gallery"></i> <?php esc_attr_e('Gallery Images', 'slideshow'); ?></button>
             <button type="button" class="button button-secondary"><i class="dashicons dashicons-trash"></i> <?php esc_attr_e('Remove Gallery Images', 'slideshow'); ?></button><br/>
         </td>
     </tr>
