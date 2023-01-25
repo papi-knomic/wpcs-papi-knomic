4b5d4cd7bc9525ba6565cbe841e31f15c3e01ac3
Samson Moses <knomic10@gmail.com>
2023-01-25 22:00:57 +0100

    Created tests for init and settings class

--- tests/init-test-class.php
+++ tests/init-test-class.php
@@ -1,1 +1,35 @@
 <?php
+
+class InitTest extends WP_UnitTestCase {
+
+	public function test_get_services() {
+		$services = Init::get_services();
+		$this->assertEquals( $services, [
+			Settings::class,
+			Admin::class,
+			Enqueue::class,
+			Frontend::class,
+			Ajax::class
+		]);
+	}
+
+	public function test_register_services() {
+		$mock = $this->getMockBuilder( Settings::class )
+		             ->setMethods( [ 'register' ] )
+		             ->getMock();
+		$mock->expects( $this->once() )
+		     ->method( 'register' );
+		$services = [
+			$mock
+		];
+		$this->setProtectedProperty( Init::class, 'services', $services );
+		Init::register_services();
+	}
+
+	public function test_instantiate() {
+		$this->setProtectedMethod( 'instantiate' );
+		$instance = $this->instantiate( Settings::class );
+		$this->assertInstanceOf( Settings::class, $instance );
+	}
+}
+
--- tests/settings-test-class.php
+++ tests/settings-test-class.php
@@ -1,1 +1,30 @@
 <?php
+
+require '../class.settings.php';
+
+class SettingsTest extends WP_UnitTestCase {
+
+	/**
+	 * @var Settings
+	 */
+	private $settings;
+
+	public function setUp() {
+		parent::setUp();
+		$this->settings = new Settings();
+	}
+
+	public function test_register() {
+		$settings = $this->settings;
+		$settings->register();
+		$this->assertEquals( has_action( 'after_setup_theme', [ $settings, 'set_image_sizes' ] ), 10 );
+	}
+
+	public function test_set_image_sizes() {
+		$settings = $this->settings;
+		$settings->set_image_sizes();
+		$this->assertTrue( current_theme_supports( 'post-thumbnails' ) );
+		$this->assertTrue( has_image_size( KNOMIC_SLIDESHOW__LARGE ) );
+		$this->assertTrue( has_image_size( KNOMIC_SLIDESHOW__THUMB ) );
+	}
+}
